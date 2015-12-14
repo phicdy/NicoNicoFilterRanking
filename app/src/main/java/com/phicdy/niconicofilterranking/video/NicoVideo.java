6999d5592b42e86cbaede527b1d2fdb86ebf14e2
phicdy <phicdy@gmail.com>
2015-12-15 00:08:09 +0900

    Add class for nico video

---
+++ app/src/main/java/com/phicdy/niconicofilterranking/video/NicoVideo.java
@@ -0,0 +1,78 @@
+package com.phicdy.niconicofilterranking.video;
+
+public class NicoVideo {
+
+    private String title;
+    private String url;
+    private String playCount;
+    private String commentCount;
+    private String myListCount;
+    private String publishedDate;
+    private String thumbnailPath;
+
+    public NicoVideo(String title, String url, String playCount, String commentCount, String myListCount, String publishedDate, String thumbnailPath) {
+        this.title = title;
+        this.url = url;
+        this.playCount = playCount;
+        this.commentCount = commentCount;
+        this.myListCount = myListCount;
+        this.publishedDate = publishedDate;
+        this.thumbnailPath = thumbnailPath;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(String publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPlayCount() {
+        return playCount;
+    }
+
+    public void setPlayCount(String playCount) {
+        this.playCount = playCount;
+    }
+
+    public String getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(String commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public String getMyListCount() {
+        return myListCount;
+    }
+
+    public void setMyListCount(String myListCount) {
+        this.myListCount = myListCount;
+    }
+
+    public String getThumbnailPath() {
+        return thumbnailPath;
+    }
+
+    public void setThumbnailPath(String thumbnailPath) {
+        this.thumbnailPath = thumbnailPath;
+    }
+}
