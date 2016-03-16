11d1f3eaf317a809a9121a7c603fc4d1244b7b11
phicdy <phicdy@gmail.com>
2016-03-17 00:10:38 +0900

    Add test class for RssParser

---
+++ app/src/test/java/com/phicdy/niconicofilterranking/rss/NicoChartFeed.java
@@ -0,0 +1,149 @@
+package com.phicdy.niconicofilterranking.rss;
+
+import com.phicdy.niconicofilterranking.video.NicoVideo;
+
+public class NicoChartFeed {
+
+    public static final int ENTRY_SIZE = 10;
+    public static final NicoVideo FIRST_VIDEO = new NicoVideo(
+            "第1位：出だしから一発でノックアウトされるアニメOP集",
+            "http://www.nicovideo.jp/watch/sm28414549",
+            "217,627",
+            "10,278",
+            "8,609",
+            "2016-03-13T20:33:40+09:00",
+            "http://tn-skr2.smilevideo.jp/smile?i=28414549");
+
+    public static final String FEED = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<feed xmlns=\"http://www.w3.org/2005/Atom\" xml:lang=\"ja\">\n" +
+            "  <title>合算の総合ランキング(デイリー)‐ニコニコチャート</title>\n" +
+            "  <subtitle>毎日午前5時更新</subtitle>\n" +
+            "  <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicochart.jp/ranking/\"/>\n" +
+            "  <link rel=\"self\" type=\"application/atom+xml\" href=\"http://www.nicochart.jp/ranking/feed\"/>\n" +
+            "  <id>tag:nicovideo.jp:/ranking/</id>\n" +
+            "  <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "  <author><name>ニコニコチャート</name></author>\n" +
+            "  <generator uri=\"http://www.nicochart.jp/\">ニコニコチャート</generator>\n" +
+            "  <rights>nicochart all rights reserved.</rights>\n" +
+            "  <entry>\n" +
+            "    <title>第1位：出だしから一発でノックアウトされるアニメOP集</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28414549\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-13:/watch/sm28414549</id>\n" +
+            "    <published>2016-03-13T20:33:40+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"出だしから一発でノックアウトされるアニメOP集\" src=\"http://tn-skr2.smilevideo.jp/smile?i=28414549\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">■※アニメを見ている中でうｐ主がアニメのOPの出だしのイントロ、または出だしのサビの部分を聴いて一発でノックアウトつまり一回でハマってしまった曲を厳選して集めてみました。あくまで個人的にそう感じた曲を集めたので予めご了承ください。年代的には縛りはありませんが2000年代以降の曲が多く王道のアニソンは結構入ってると思います。追記　まさかここまで伸びてるとは・・・。近日中にPart2うｐ出来ればと思います。■まとめ→mylist/39077091</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">304,238</strong>pts.｜<strong class=\"nico-info-length\">34:17</strong>｜<strong class=\"nico-info-date\">2016年03月13日 20:33:40</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">217,627</strong>　コメント：<strong class=\"nico-info-total-res\">10,278</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">8,609</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">187,023</strong>　コメント：<strong class=\"nico-info-daily-res\">8,426</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">7,256</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第2位：おしえて！ギャル子ちゃん　第8話「ヒドい寝ぐせって本当ですか？」</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28391022\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-14:/watch/so28391022</id>\n" +
+            "    <published>2016-03-14T23:30:00+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"おしえて！ギャル子ちゃん　第8話「ヒドい寝ぐせって本当ですか？」\" src=\"http://tn-skr3.smilevideo.jp/smile?i=28391022\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">クラスの女子の中で誰が一番胸が大きいのかを議論するチャラ男、スポ男、オタ男の三人。机にのっているギャル子の胸を見て、問答無用でギャル子だろと言うチャラ男に対して、スポ男はサイズなら肉子もすごいと言い、議論は白熱していく。原作コミックが今すぐ読める！動画一覧はこちら第7話 watch/1456980131</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">220,680</strong>pts.｜<strong class=\"nico-info-length\">7:50</strong>｜<strong class=\"nico-info-date\">2016年03月14日 23:30:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">240,171</strong>　コメント：<strong class=\"nico-info-total-res\">10,477</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">1,576</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">196,490</strong>　コメント：<strong class=\"nico-info-daily-res\">8,002</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">1,054</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第3位：IKE◯の椅子は本当に壊れる？ユーザー記者もこうがあらゆる技で検証!!</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28398087\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-15:/watch/sm28398087</id>\n" +
+            "    <published>2016-03-15T10:00:00+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"IKE◯の椅子は本当に壊れる？ユーザー記者もこうがあらゆる技で検証!!\" src=\"http://tn-skr4.smilevideo.jp/smile?i=28398087\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">某お笑い芸人が生放送中に破壊してしまった”IKE○の椅子”。なんでも630万回の耐久テストに合格した堅牢な椅子だそうですが、実際、例の番組ではあっけなく壊れてしまいました。その耐久性は本物なのか？改めて僕が検証してみました！！mylist/29936399</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">164,090</strong>pts.｜<strong class=\"nico-info-length\">7:08</strong>｜<strong class=\"nico-info-date\">2016年03月15日 10:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">150,365</strong>　コメント：<strong class=\"nico-info-total-res\">5,151</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">535</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">150,365</strong>　コメント：<strong class=\"nico-info-daily-res\">5,151</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">535</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第4位：【ウオミー】英稜高校生徒会長活動記【生徒会役員共＊】</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm23223010\"/>\n" +
+            "    <id>tag:nicovideo.jp,2014-04-01:/watch/sm23223010</id>\n" +
+            "    <published>2014-04-01T00:31:00+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"【ウオミー】英稜高校生徒会長活動記【生徒会役員共＊】\" src=\"http://tn-skr3.smilevideo.jp/smile?i=23223010\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">ウオミーの出番少ないなーと思ってたがこうして見ると結構出番多いねウオミーまとめはほかの人が先に上げてますが個人的に1つの動画にまとまってる方が見やすいので自分用のためにも上げましたその他投稿したものmylist/33074632</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">161,327</strong>pts.｜<strong class=\"nico-info-length\">29:00</strong>｜<strong class=\"nico-info-date\">2014年04月01日 00:31:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">249,717</strong>　コメント：<strong class=\"nico-info-total-res\">2,637</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">5,459</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">111,430</strong>　コメント：<strong class=\"nico-info-daily-res\">1,266</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">3,203</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第5位：しょぼんのアクション -迅-　実況プレイ　1　(´・ω・｀)</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28427839\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-15:/watch/sm28427839</id>\n" +
+            "    <published>2016-03-15T19:17:37+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"しょぼんのアクション -迅-　実況プレイ　1　(´・ω・｀)\" src=\"http://tn-skr4.smilevideo.jp/smile?i=28427839\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">しょぼんのアクション(´・ω・｀)の作者様によるスマホオリジナルステージ！相変わらずの初見殺しやでぇ…。コメントが打てない人はコチラ→sm28427839高画質版https://youtu.be/NpuHZf72EzEしょぼん→mylist/31178341｜次回→***twitter：http://twitter.com/abuuuntter　実況まとめ：mylist/13083520</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">148,770</strong>pts.｜<strong class=\"nico-info-length\">7:08</strong>｜<strong class=\"nico-info-date\">2016年03月15日 19:17:37</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">109,546</strong>　コメント：<strong class=\"nico-info-total-res\">1,228</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">934</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">109,546</strong>　コメント：<strong class=\"nico-info-daily-res\">1,228</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">934</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第6位：【実況】少女と殺人鬼の奇妙な絆と“イカれた約束”【Part26 完】</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28423718\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-15:/watch/so28423718</id>\n" +
+            "    <published>2016-03-15T18:00:00+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"【実況】少女と殺人鬼の奇妙な絆と“イカれた約束”【Part26 完】\" src=\"http://tn-skr3.smilevideo.jp/smile?i=28423718\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">最終回。ご視聴ありがとうございました。　　コメントできない方は→watch/1457967491「殺戮の天使」をプレイして脱出するぜ。公式サイト→http://info.nicovideo.jp/gamemaga/satsuriku/※他の方に迷惑をかけないようにご視聴よろしくお願いします。チャンネルからの投稿ですが誰でも最後まで視聴できます。Part1→watch/1439600999　　前→watch/1457690972　　マイリスト→mylist/51970332コミュ→co217323実況プレイpart1リンク・新作→mylist/15923690　　個人実況まとめ→mylist/25740145　mylist/34700173【ツイッター】　キヨ→http://twitter.com/kiyo_saiore　（動画・更新情報など）【YouTube】　http://t.co/8FhNlN2wIy</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">145,393</strong>pts.｜<strong class=\"nico-info-length\">19:33</strong>｜<strong class=\"nico-info-date\">2016年03月15日 18:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">79,155</strong>　コメント：<strong class=\"nico-info-total-res\">11,522</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">3,368</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">79,155</strong>　コメント：<strong class=\"nico-info-daily-res\">11,519</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">3,368</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第7位：おそ松さん　第22話「A「希望の星、トド松」　B「ファイナルシェー」」</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28384812\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-14:/watch/so28384812</id>\n" +
+            "    <published>2016-03-14T00:30:00+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"おそ松さん　第22話「A「希望の星、トド松」　B「ファイナルシェー」」\" src=\"http://tn-skr1.smilevideo.jp/smile?i=28384812\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">「ファイナルシェー」とある国の拳法の師範代が、母国の危機を救うためある男に目をつけた。はたしてそれは？？動画一覧はこちら第21話 watch/1456894464</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">130,706</strong>pts.｜<strong class=\"nico-info-length\">23:50</strong>｜<strong class=\"nico-info-date\">2016年03月14日 00:30:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">405,352</strong>　コメント：<strong class=\"nico-info-total-res\">41,003</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">3,393</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">113,735</strong>　コメント：<strong class=\"nico-info-daily-res\">9,302</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">548</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第8位：ダイエットしてイケメンどもを見返す【実況】part5</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28424589\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-15:/watch/sm28424589</id>\n" +
+            "    <published>2016-03-15T05:54:16+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"ダイエットしてイケメンどもを見返す【実況】part5\" src=\"http://tn-skr2.smilevideo.jp/smile?i=28424589\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">ダイエット恋愛ゲーム『乙女的恋革命★ラブレボ!!』の実況プレイです。絶対旅行行くマン・・・。前⇒sm28362079　　　次⇒近日　　　mylist/55081433　　　コメントできない方は⇒sm28424589★実況part1集⇒mylist/16254406ツイッター（更新予定や生放送の情報など）⇒http://twitter.com/retokani 　　　　コミュ⇒co22673youtubeチャンネル⇒https://www.youtube.com/user/retokani</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">119,560</strong>pts.｜<strong class=\"nico-info-length\">27:05</strong>｜<strong class=\"nico-info-date\">2016年03月15日 05:54:16</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">86,160</strong>　コメント：<strong class=\"nico-info-total-res\">8,034</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">1,702</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">86,160</strong>　コメント：<strong class=\"nico-info-daily-res\">8,034</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">1,702</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第9位：【伝説サッカー史に残るゴール】 ヘアレス岡崎慎司バイシクルゴール</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28425103\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-15:/watch/sm28425103</id>\n" +
+            "    <published>2016-03-15T05:40:44+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"【伝説サッカー史に残るゴール】 ヘアレス岡崎慎司バイシクルゴール\" src=\"http://tn-skr4.smilevideo.jp/smile?i=28425103\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">15-16プレミア 第30節 レスター・シティ vs ニューカッスル【完全版】 岡崎慎司 Top 20 Goals sm28371837【投稿者情報】【コミュニティ】co3057965、co2704585【サッカー動画】mylist/53921711、mylist/50082763【twiter】https://twitter.com/niko2525_soccer</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">112,741</strong>pts.｜<strong class=\"nico-info-length\">1:16</strong>｜<strong class=\"nico-info-date\">2016年03月15日 05:40:44</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">105,459</strong>　コメント：<strong class=\"nico-info-total-res\">429</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">437</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">105,459</strong>　コメント：<strong class=\"nico-info-daily-res\">429</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">437</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第10位：大家さんは思春期！　第10話「大家さんは成長中？」</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28397476\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-03-14:/watch/so28397476</id>\n" +
+            "    <published>2016-03-14T22:00:00+09:00</published>\n" +
+            "    <updated>2016-03-16T05:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"大家さんは思春期！　第10話「大家さんは成長中？」\" src=\"http://tn-skr1.smilevideo.jp/smile?i=28397476\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">アパートに勉強会にやってきた真由と雪を見かけた麗子は、雪の体の異変に気づく。成長期の雪はブラのサイズが合っていなかったのだ…。胸の大きなもの同士で共感する二人を冷ややかに見るチエと真由だったが…。原作コミックが今すぐ読める！！動画一覧はこちら第9話 watch/1457067547</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">108,294</strong>pts.｜<strong class=\"nico-info-length\">2:00</strong>｜<strong class=\"nico-info-date\">2016年03月14日 22:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">142,647</strong>　コメント：<strong class=\"nico-info-total-res\">2,188</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">982</strong><br/><strong>日間</strong>　再生：<strong class=\"nico-info-daily-view\">99,734</strong>　コメント：<strong class=\"nico-info-daily-res\">1,195</strong>　マイリスト：<strong class=\"nico-info-daily-mylist\">404</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "</feed>\n";
+}
---
+++ app/src/test/java/com/phicdy/niconicofilterranking/rss/RssParserTest.java
@@ -0,0 +1,34 @@
+package com.phicdy.niconicofilterranking.rss;
+
+import com.phicdy.niconicofilterranking.video.NicoVideo;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnit4.class)
+public class RssParserTest{
+
+    @Test
+    public void parse() throws Exception {
+        Document document = Jsoup.parse(NicoChartFeed.FEED);
+        RssParser parser = new RssParser();
+        ArrayList<NicoVideo> videos = parser.parseNicoChartFeed(document);
+        assertThat(videos.size(), is(NicoChartFeed.ENTRY_SIZE));
+
+        NicoVideo video = videos.get(0);
+        assertThat(video.getTitle(), is(NicoChartFeed.FIRST_VIDEO.getTitle()));
+        assertThat(video.getUrl(), is(NicoChartFeed.FIRST_VIDEO.getUrl()));
+        assertThat(video.getPlayCount(), is(NicoChartFeed.FIRST_VIDEO.getPlayCount()));
+        assertThat(video.getCommentCount(), is(NicoChartFeed.FIRST_VIDEO.getCommentCount()));
+        assertThat(video.getMyListCount(), is(NicoChartFeed.FIRST_VIDEO.getMyListCount()));
+        assertThat(video.getThumbnailPath(), is(NicoChartFeed.FIRST_VIDEO.getThumbnailPath()));
+    }
+}
