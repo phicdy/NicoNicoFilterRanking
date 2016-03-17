cdda8851c6ada54725a9c6259e9539c6f830c36d
phicdy <phicdy@gmail.com>
2016-03-17 22:51:41 +0900

    Use support Fragment

--- app/src/main/java/com/phicdy/niconicofilterranking/ui/NicoVideoListFragment.java
+++ app/src/main/java/com/phicdy/niconicofilterranking/ui/NicoVideoListFragment.java
@@ -3,7 +3,7 @@ package com.phicdy.niconicofilterranking.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
