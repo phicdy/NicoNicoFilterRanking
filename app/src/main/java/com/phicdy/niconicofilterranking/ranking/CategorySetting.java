bb5a441a9a928e3728aa0c7c9315caf34e4fd0ee
phicdy <phicdy@gmail.com>
2016-04-23 12:16:16 +0900

    Let CategorySetting have map to manage availability of each category

--- app/src/main/java/com/phicdy/niconicofilterranking/ranking/CategorySetting.java
+++ app/src/main/java/com/phicdy/niconicofilterranking/ranking/CategorySetting.java
@@ -1,4 +1,43 @@
 package com.phicdy.niconicofilterranking.ranking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CategorySetting {
+    Map<Integer, Boolean> availabilityMap = new HashMap<>();
+
+    public CategorySetting() {
+        int[] allCategories = Category.getAllCategories();
+        for (int category : allCategories) {
+            availabilityMap.put(category, false);
+        }
+    }
+
+    public void setAvailability(int category, boolean enabled) {
+        availabilityMap.put(category, enabled);
+    }
+
+    public boolean getAvailability(int category) {
+        return availabilityMap.get(category);
+    }
+
+    /**
+     *
+     * @param o Instance of CategorySetting class
+     * @return Return {@code true} if o is instance of CategorySetting class and
+     * all of the category availability equals myself. Else return {@code false}
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof CategorySetting)) {
+            return false;
+        }
+        int[] allCategories = Category.getAllCategories();
+        for (int category : allCategories) {
+            if (((CategorySetting) o).getAvailability(category) != getAvailability(category)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
