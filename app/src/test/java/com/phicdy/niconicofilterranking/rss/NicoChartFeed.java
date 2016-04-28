4a932166ebd2171017b14cbe9e16fc83018ca2b7
phicdy <phicdy@gmail.com>
2016-04-28 21:06:44 +0900

    Add test data

--- app/src/test/java/com/phicdy/niconicofilterranking/rss/NicoChartFeed.java
+++ app/src/test/java/com/phicdy/niconicofilterranking/rss/NicoChartFeed.java
@@ -87,4 +87,152 @@ public class NicoChartFeed {
             "    ]]></content>\n" +
             "  </entry>\n" +
             "</feed>\n";
+
+    public static String FEED2 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<feed xmlns=\"http://www.w3.org/2005/Atom\" xml:lang=\"ja\">\n" +
+            "  <title>合算の総合ランキング(毎時)‐ニコニコチャート</title>\n" +
+            "  <subtitle>毎時更新</subtitle>\n" +
+            "  <link rel=\"alternate\" type=\"text/html\" href=\"http://now.nicochart.jp/hourly/\"/>\n" +
+            "  <link rel=\"self\" type=\"application/atom+xml\" href=\"http://now.nicochart.jp/hourly/feed\"/>\n" +
+            "  <id>tag:nicovideo.jp:/hourly/</id>\n" +
+            "  <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "  <author><name>ニコニコチャート</name></author>\n" +
+            "  <generator uri=\"http://www.nicochart.jp/\">ニコニコチャート</generator>\n" +
+            "  <rights>nicochart all rights reserved.</rights>\n" +
+            "  <entry>\n" +
+            "    <title>第1位：幕末志士達のダウンタウン熱血物語実況プレイ</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28728573\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-28:/watch/sm28728573</id>\n" +
+            "    <published>2016-04-28T18:35:01+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"幕末志士達のダウンタウン熱血物語実況プレイ\" src=\"http://tn-skr2.smilevideo.jp/smile?i=28728573\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">さらわれた女の子を助けに、くにおとりきが旅をするゲームです。1P(くにお):坂本、2P(りき):西郷sm28399022　←　前　次　→　5/30【３０周年企画】幕末志士達のくにおくん実況スペシャルhttp://www.kuniokun.jp/bakumatsu/ゲーム実況まとめ : mylist/8299531更新情報(Twitter)　→　https://twitter.com/kirizaki_ei単発動画まとめ : mylist/10539272生放送まとめ : mylist/53482635幕末志士チャンネル　: ch.nicovideo.jp/bakumatsu</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">14,268</strong>pts.｜<strong class=\"nico-info-length\">22:43</strong>｜<strong class=\"nico-info-date\">2016年04月28日 18:35:01</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">15,666</strong>　コメント：<strong class=\"nico-info-total-res\">786</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">435</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">10,430</strong>　コメント：<strong class=\"nico-info-hourly-res\">436</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">228</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第2位：【実況】屋敷に秘められた真実も知らずに――『狂い月』【Part7】</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28729035\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-28:/watch/sm28729035</id>\n" +
+            "    <published>2016-04-28T19:37:14+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"【実況】屋敷に秘められた真実も知らずに――『狂い月』【Part7】\" src=\"http://tn-skr4.smilevideo.jp/smile?i=28729035\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">足を破壊しておけ　　コメントできない方は→sm28729035「狂い月」を実況じゃい。公式HP⇒⇒http://3colorsbread.web.fc2.com/kuruiduki/※他の方に迷惑をかけないようにご視聴よろしくお願いします。Part1→sm28559373　　前→sm28701719　　次→早めの更新　　マイリスト→mylist/55419189コミュ→co217323実況プレイpart1リンク・新作→mylist/15923690　　個人実況まとめ→mylist/25740145　mylist/34700173【ツイッター】　キヨ→http://twitter.com/kiyo_saiore　（動画・更新情報など）【YouTube】　http://t.co/8FhNlN2wIy</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">9,940</strong>pts.｜<strong class=\"nico-info-length\">19:03</strong>｜<strong class=\"nico-info-date\">2016年04月28日 19:37:14</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">6,214</strong>　コメント：<strong class=\"nico-info-total-res\">744</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">204</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">6,214</strong>　コメント：<strong class=\"nico-info-hourly-res\">744</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">204</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第3位：クソゲー10選FINAL 【PS1】</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28725932\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-28:/watch/sm28725932</id>\n" +
+            "    <published>2016-04-28T06:00:00+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"クソゲー10選FINAL 【PS1】\" src=\"http://tn-skr1.smilevideo.jp/smile?i=28725932\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">プレステ１のクソゲー動画もいよいよ最後です。まだ有るけどこのくらいで良いよね。【YouTube：HD画質版リンク】ar611772 ←関連動画もこちら【続クソゲー10選 PS編】sm27887869【クソゲー10選 PS編】sm25126683【奇妙ゲーム集 PS編】sm25007080【セガサターンお薦めソフト地獄級】sm26582831【 FIST比較 】sm23589650【お蔵入りゲーム PS編】sm24343797【お蔵入りゲーム SS編】sm24185456【PS vs SS】sm23455584【適当動画リスト】mylist/14485308【百本動画】mylist/14279566【UP主のニコ生コミュ：やわらか宇宙研究所(仮)】co325473</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">8,655</strong>pts.｜<strong class=\"nico-info-length\">14:14</strong>｜<strong class=\"nico-info-date\">2016年04月28日 06:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">34,924</strong>　コメント：<strong class=\"nico-info-total-res\">1,181</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">250</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">7,855</strong>　コメント：<strong class=\"nico-info-hourly-res\">143</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">44</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第4位：【Minecraft】 方向音痴のマインクラフト Season6 Part8 【ゆっくり実況】</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28720817\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-28:/watch/sm28720817</id>\n" +
+            "    <published>2016-04-28T01:00:00+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"【Minecraft】 方向音痴のマインクラフト Season6 Part8 【ゆっくり実況】\" src=\"http://tn-skr2.smilevideo.jp/smile?i=28720817\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">エンチャントから地下渓谷へ前：sm28686877　次：まだ　再生リスト：mylist/23976036※Part8裏話：http://bellsworkshop.com/season6-part8ブログ&nbsp;&nbsp;&nbsp;http://www.bellsworkshop.com/Youtubeチャンネル&nbsp;&nbsp;&nbsp;http://www.youtube.com/user/bellsworkshop &nbsp;&nbsp;(WoW、WoT他)&nbsp;&nbsp;&nbsp;http://www.youtube.com/user/bellsworkshop2 (Minecraft他)Music：purple planet royalty free music　http://www.purple-planet.com/Sound：On-Jin ～音人～　http://on-jin.com/</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">7,396</strong>pts.｜<strong class=\"nico-info-length\">11:03</strong>｜<strong class=\"nico-info-date\">2016年04月28日 01:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">79,640</strong>　コメント：<strong class=\"nico-info-total-res\">1,035</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">1,388</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">6,210</strong>　コメント：<strong class=\"nico-info-hourly-res\">32</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">77</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第5位：弱酸性ミリオンアーサー #24</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28720768\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-27:/watch/so28720768</id>\n" +
+            "    <published>2016-04-27T14:16:15+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"弱酸性ミリオンアーサー #24\" src=\"http://tn-skr1.smilevideo.jp/smile?i=28720768\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">名勝負　相撲大会秋場所の末に　困惑◆原作：ちょぼらうにょぽみ◆制作：ギャザリング（株）◆製作：（株）スクウェア・エニックス◆監督：まんきゅう前のお話はこちら →watch/1461119824動画一覧はこちら■原作の4コマ漫画は『乖離性ミリオンアーサー』アプリ内で読むことができます！iPhoneの方はこちらからダウンロードAndroidの方はこちらからダウンロード</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">7,193</strong>pts.｜<strong class=\"nico-info-length\">1:30</strong>｜<strong class=\"nico-info-date\">2016年04月27日 14:16:15</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">205,290</strong>　コメント：<strong class=\"nico-info-total-res\">2,630</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">3,188</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">6,046</strong>　コメント：<strong class=\"nico-info-hourly-res\">53</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">73</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>" +
+            "</feed>\n";
+
+    public static String FEED3 = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<feed xmlns=\"http://www.w3.org/2005/Atom\" xml:lang=\"ja\">\n" +
+            "  <title>エンターテイメントの総合ランキング(毎時)‐ニコニコチャート</title>\n" +
+            "  <subtitle>毎時更新</subtitle>\n" +
+            "  <link rel=\"alternate\" type=\"text/html\" href=\"http://now.nicochart.jp/hourly/ent\"/>\n" +
+            "  <link rel=\"self\" type=\"application/atom+xml\" href=\"http://now.nicochart.jp/hourly/ent/feed\"/>\n" +
+            "  <id>tag:nicovideo.jp:/hourly/ent</id>\n" +
+            "  <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "  <author><name>ニコニコチャート</name></author>\n" +
+            "  <generator uri=\"http://www.nicochart.jp/\">ニコニコチャート</generator>\n" +
+            "  <rights>nicochart all rights reserved.</rights>\n" +
+            "  <entry>\n" +
+            "    <title>第1位：【もこうMAD】存在が武勇伝な男【PERFECT HUMAN】</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28679356\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-21:/watch/sm28679356</id>\n" +
+            "    <published>2016-04-21T18:00:00+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"【もこうMAD】存在が武勇伝な男【PERFECT HUMAN】\" src=\"http://tn-skr1.smilevideo.jp/smile?i=28679356\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">sm28571216にハマってもこうverが見たかったので本家様みたいなクオリティはないですが(;⌒,_ゝ⌒)許せよ…マイリス→mylist/52713934宣伝ありがとうございます！</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">1,861</strong>pts.｜<strong class=\"nico-info-length\">3:39</strong>｜<strong class=\"nico-info-date\">2016年04月21日 18:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">351,402</strong>　コメント：<strong class=\"nico-info-total-res\">4,425</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">8,557</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">1,360</strong>　コメント：<strong class=\"nico-info-hourly-res\">22</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">32</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第2位：シーサ。の回胴日記_第４７７話[by ARROWS-SCREEN]</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28721482\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-27:/watch/so28721482</id>\n" +
+            "    <published>2016-04-27T18:00:00+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"シーサ。の回胴日記_第４７７話[by ARROWS-SCREEN]\" src=\"http://tn-skr3.smilevideo.jp/smile?i=28721482\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">シーサ。が福岡県「つかさ月隈店」で実戦！朝から新台を抑えたシーサ。！！序盤良い流れを掴むが最後には…！？収録日：2016年4月2日（土）また、１０話ごとの節目では番組内でお便りコーナーを設けております！アロウズスクリーンへのご要望、シーサ。への質問など、何でもお気軽にcomment@arrows-screen.comまでどしどしご応募下さいませ！！ その他配信動画の情報はこちら！！アロウズスクリーンＨＰブログtwitter@arrowsscreenFacebookシーサ。の回胴日記(１～４３４話)mylist/35716253シーサ。の回胴日記(４３５話～)mylist/54472185</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">962</strong>pts.｜<strong class=\"nico-info-length\">36:33</strong>｜<strong class=\"nico-info-date\">2016年04月27日 18:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">38,713</strong>　コメント：<strong class=\"nico-info-total-res\">2,928</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">109</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">925</strong>　コメント：<strong class=\"nico-info-hourly-res\">23</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">1</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第3位：モコウ・カテ・ナイツ</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm28097713\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-01-28:/watch/sm28097713</id>\n" +
+            "    <published>2016-01-28T18:06:44+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"モコウ・カテ・ナイツ\" src=\"http://tn-skr2.smilevideo.jp/smile?i=28097713\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">(#⌒,_ゝ⌒)キレなきゃむしろ人間じゃねぇmylist/52713934りむるんさん、ぽっぷさん、ゲストさん、りめるんさん、加藤純一さん、セッフィロスさん、belunaさん、高田建志さん、馬場 豊さん、ウィッチさん、りめるんさん、りめるんともこうの息子さん、( 　ﾟ,_ゝﾟ)さん、大和魂さん、レトルトさん、Wii U GamePadさん、ひななぃさん、ムツゴロウが殴った猿さん、万年落第受講生Yさん、アブ4ねさん、くろむさん、われもこうさん、ヒルナンデスで値切ろうとした男さん、サンタさん、-CORE2-さん、フレデリック・エスポワールさん、もっこもこさん、大和魂さん、智明さん、内田真礼さん、エートスさん、りめるんさん、上野動物園さん、yakouさん、チンパンジーさん、キヨさん、つわはすさん他たくさんの宣伝ありがとうございます！</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">793</strong>pts.｜<strong class=\"nico-info-length\">3:15</strong>｜<strong class=\"nico-info-date\">2016年01月28日 18:06:44</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">1,027,605</strong>　コメント：<strong class=\"nico-info-total-res\">36,278</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">23,035</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">577</strong>　コメント：<strong class=\"nico-info-hourly-res\">22</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">13</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第4位：だいたい6分でわかるニコニコ超会議2016</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/so28671739\"/>\n" +
+            "    <id>tag:nicovideo.jp,2016-04-20:/watch/so28671739</id>\n" +
+            "    <published>2016-04-20T13:00:00+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"だいたい6分でわかるニコニコ超会議2016\" src=\"http://tn-skr4.smilevideo.jp/smile?i=28671739\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">超会議2016マップを片手に一緒に探索しよう！幕張メッセ１～11ホールに加え、イベントホール、QVCマリンフィールドを加えた、史上最大のニコニコ超会議2016。開催までのあいだ、マップ片手に探索して企画をおさらいしよう！■ニコニコ超会議2016■　4月29日（金・祝）、30日（土）千葉・幕張メッセ国際展示場1〜11ホール、イベントホールhttp://www.chokaigi.jp/＜前売り入場券だと500円もお得＞入場券は各種プレイガイドで好評発売中http://www.chokaigi.jp/2016/ticket/</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">792</strong>pts.｜<strong class=\"nico-info-length\">6:00</strong>｜<strong class=\"nico-info-date\">2016年04月20日 13:00:00</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">69,976</strong>　コメント：<strong class=\"nico-info-total-res\">6,057</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">495</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">684</strong>　コメント：<strong class=\"nico-info-hourly-res\">52</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">4</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>\n" +
+            "    <title>第5位：マグロが来た！</title>\n" +
+            "    <link rel=\"alternate\" type=\"text/html\" href=\"http://www.nicovideo.jp/watch/sm13051173\"/>\n" +
+            "    <id>tag:nicovideo.jp,2010-12-17:/watch/sm13051173</id>\n" +
+            "    <published>2010-12-17T21:15:19+09:00</published>\n" +
+            "    <updated>2016-04-28T20:00:00+09:00</updated>\n" +
+            "    <content type=\"html\"><![CDATA[\n" +
+            "      <p class=\"nico-thumbnail\"><img alt=\"マグロが来た！\" src=\"http://tn-skr2.smilevideo.jp/smile?i=13051173\" width=\"94\" height=\"70\" border=\"0\"/></p>\n" +
+            "      <p class=\"nico-description\">＼ドドーン／</p>\n" +
+            "      <p class=\"nico-info\"><small><strong class=\"nico-info-number\">466</strong>pts.｜<strong class=\"nico-info-length\">0:57</strong>｜<strong class=\"nico-info-date\">2010年12月17日 21:15:19</strong> 投稿<br/><strong>合計</strong>　再生：<strong class=\"nico-info-total-view\">39,027</strong>　コメント：<strong class=\"nico-info-total-res\">273</strong>　マイリスト：<strong class=\"nico-info-total-mylist\">958</strong><br/><strong>毎時</strong>　再生：<strong class=\"nico-info-hourly-view\">391</strong>　コメント：<strong class=\"nico-info-hourly-res\">1</strong>　マイリスト：<strong class=\"nico-info-hourly-mylist\">5</strong><br/></small></p>\n" +
+            "    ]]></content>\n" +
+            "  </entry>\n" +
+            "  <entry>" +
+            "</feed>\n";
 }
